16bc03d6bf3f2a059861b3f50bf041f397391985
Flyard <ranaivosonflorian@gmail.com>
2023-02-15 09:49:00 +0100

    sort by brand

--- client/v2/clean-fashion-frontend/src/components/Home.vue
+++ client/v2/clean-fashion-frontend/src/components/Home.vue
@@ -7,47 +7,69 @@
         <option>48</option>
     </select>
 
-    <button type="button" v-on:click="byReasonablePrice()">By reasonable price</button>
-    <button type="button" v-on:click="byReleased()">By recently released</button>
+    <p>By brand:</p>
+    <select v-model="brandSelect">
+        <option disabled>Choose a brand</option>
+        <option v-for="brands in brands" >{{ brands }}</option>
+    </select>
+
+
 
     <h1>Indicators</h1>
 
     <h1>Products</h1>
-    <p v-for="products in products">{{ products.brand + ' ' + products.name + ' ' + products.price}}</p>
+    
+    <div v-for="products in products">
+        <li v-if="sortBrand(brandSelect,products.brand)">
+        {{ products.brand + ' '+products.name + ' ' + products.price }} 
+        </li>
+    </div>
+
+
+
+
+    
 
 
 </template>
 
 <script>
-    import axios from 'axios';
+import axios from 'axios';
     const url = 'https://clear-fashion-api.vercel.app/'
     export default {
         data() {
             return {
-                brandSelection: '',
                 products: [],
                 size: 0,
+                brands: [],
+                brandSelect: ''
         }
         },
         created() {
-            this.fetchData()
         },
         watch: {
-            size: 'fetchData'
+            size: 'fetchData',
+            brandSelect: 'sortBrand'
         },
         methods: {
             async fetchData() {
                 let data = await axios.get(url+`?size=${this.size}`)
                 .then((res) => {
-                    this.products = res.data.data.result               
+                    this.products = res.data.data.result;
+                    let brand = axios.get(url+'brands')
+                    .then((response) => {
+                        this.brands = response.data.data.result;
+
+                    })               
                 })
                 .catch((error) => {
                     console.log(error)
                 })
             },
 
+            sortBrand(brandSelect, products) { 
+                return brandSelect === products;
+            }
         },
-
-
     }
 </script>
