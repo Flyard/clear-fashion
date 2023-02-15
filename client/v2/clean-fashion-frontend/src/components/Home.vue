7e9221781ce2fab3a018c30098f3ad47d0b240f5
Flyard <ranaivosonflorian@gmail.com>
2023-02-15 23:28:50 +0100

    f6: sort by date, sort by brand, sort by price

--- client/v2/clean-fashion-frontend/src/components/Home.vue
+++ client/v2/clean-fashion-frontend/src/components/Home.vue
@@ -7,29 +7,37 @@
         <option>48</option>
     </select>
 
+    <p>Page: </p>
+    <select v-model="currentPage">
+        <option v-for="currentPage in maxPage">{{ currentPage }}</option>
+    </select>
+
+    <button type="button" v-on:click="sortDate(products)">By recently released</button>
+    <button type="button" v-on:click="sortPrice(products)">By reasonable price</button>
+
     <p>By brand:</p>
     <select v-model="brandSelect">
-        <option disabled>Choose a brand</option>
+        <option disabled value="">Choose a brand</option>
+        <option>All products</option>
         <option v-for="brands in brands" >{{ brands }}</option>
     </select>
 
-
-
     <h1>Indicators</h1>
+    <p>Number of products: {{ numberOfProducts }}</p>
+    <p>Number of brands: {{ numberOfBrands }}</p>
+    <p>Number of new products</p>
+    <p>p50 price value</p>
+    <p>p90 price value</p>
+    <p>p95 price value</p>
+    <p>Last released date</p>
 
     <h1>Products</h1>
     
     <div v-for="products in products">
-        <li v-if="sortBrand(brandSelect,products.brand)">
-        {{ products.brand + ' '+products.name + ' ' + products.price }} 
+        <li  v-if="sortBrand(brandSelect,products.brand)">
+        {{ 'Name: ' + products.name + ' Brand: '+products.brand + ' Price: ' + products.price + ' Release date: ' + products.released}} 
         </li>
     </div>
-
-
-
-
-    
-
 
 </template>
 
@@ -39,16 +47,22 @@ import axios from 'axios';
     export default {
         data() {
             return {
+                maxPage: 19,
+                currentPage: 1,
                 products: [],
                 size: 0,
                 brands: [],
-                brandSelect: ''
+                brandSelect: '',
+                numberOfProducts: 0,
+                numberOfBrands: 0,
         }
         },
         created() {
+            this.getIndicators();
         },
         watch: {
             size: 'fetchData',
+
             brandSelect: 'sortBrand'
         },
         methods: {
@@ -59,7 +73,6 @@ import axios from 'axios';
                     let brand = axios.get(url+'brands')
                     .then((response) => {
                         this.brands = response.data.data.result;
-
                     })               
                 })
                 .catch((error) => {
@@ -68,8 +81,32 @@ import axios from 'axios';
             },
 
             sortBrand(brandSelect, products) { 
-                return brandSelect === products;
+                if(brandSelect === products) { 
+                    return true;
+                } else if (brandSelect === '' || brandSelect==='All products') {return true}
+            },
+
+            sortDate(products) {
+                const sortedDateAscend = products.sort((a, b) => (Date.parse(b.released) - Date.parse(a.released)));
+                return sortedDateAscend;
+            },
+
+            sortPrice(products){
+                const sortedPriceAscend  = products.sort((a, b) => (a.price-b.price))
+                console.log(sortedPriceAscend);
+            return sortedPriceAscend;
+            },
+            async getIndicators() {
+                let nbOfProducts = await axios.get(url)
+                .then((res) => {
+                    this.numberOfProducts = res.data.data.meta.count;
+                });
+                let brand = axios.get(url+'brands')
+                    .then((response) => {
+                        this.numberOfBrands = response.data.data.result.length;
+                    })                         
             }
         },
     }
 </script>
+
